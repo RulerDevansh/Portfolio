e9b4c11b2caf9b0e33de9f872fe5ca5fb094212d
RulerDevansh <srivastavadevansh123@gmail.com>
2025-02-21 02:26:06 +0530

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import {Nav,Hero,About,TechBalls,Project, Contact, Stars} from './components'
-import {github , linkedln} from './assets'
+import { useState, useEffect } from "react";
+import { Nav, Hero, About, TechBalls, Project, Contact, Stars } from "./components";
+import { github, linkedln } from "./assets";
 
 function App() {
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    setYear(new Date().getFullYear());
+  }, []);
+
   return (
     <div>
-        {/* <h1 className="">Devansh Srivastava</h1> */}
+      {/* <h1 className="">Devansh Srivastava</h1> */}
       <Nav />
       <Hero />
       <About />
-      <TechBalls/>
-      <Project/>
+      <TechBalls />
+      <Project />
 
-      <div className='relative z-0'>
-        <Contact/>
+      <div className="relative z-0">
+        <Contact />
         <Stars />
       </div>
-    
-    <footer>
-      <div className='flex justify-evenly items-center h-8 sm:h-16 text-slate-400'>
-        <p >&copy; 2025 Devansh Srivastava</p> 
-        <div className='flex gap-4'>
-          <span>Social</span>
-          <img onClick={() => window.open("https://github.com/RulerDevansh", "_blank")} className='h-auto w-6 cursor-pointer' src={github} alt="github" />
-          <img onClick={() => window.open("https://www.linkedin.com/in/devansh-srivastava-387574297/", "_blank")} className='h-auto w-6 cursor-pointer' src={linkedln} alt="linkedln" />
+
+      <footer>
+        <div className="flex justify-evenly items-center h-8 sm:h-16 text-slate-400">
+          <p>&copy; {year} Devansh Srivastava</p>
+          <div className="flex gap-4">
+            <span>Social</span>
+            <img
+              onClick={() => window.open("https://github.com/RulerDevansh", "_blank")}
+              className="h-auto w-6 cursor-pointer"
+              src={github}
+              alt="github"
+            />
+            <img
+              onClick={() => window.open("https://www.linkedin.com/in/devansh-srivastava-387574297/", "_blank")}
+              className="h-auto w-6 cursor-pointer"
+              src={linkedln}
+              alt="linkedln"
+            />
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
